afe70ed751e389e94a63f53230be540f584db814
Morgan Jackson <morjack@siue.edu>
2023-02-17 13:29:49 -0600

    setup use of firebase api keys

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,8 +11,17 @@ import {
 } from "firebase/firestore";
 import { getAuth, signInAnonymously } from "firebase/auth";
 import { v4 as uuidv4 } from "uuid";
-import { firebaseConfig } from "../.firebase.js";
+require("dotenv").config();
+
 // Initialize Firebase
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
 initializeApp(firebaseConfig);
 
 const auth = getAuth();
