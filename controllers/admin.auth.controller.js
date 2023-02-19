e6058d6fe65a42ab8e0fc0012fd50b91ac267c6e
Morgan Jackson <morjack@siue.edu>
2023-02-19 13:47:52 -0600

    return in arrow function wasn't blocking register

--- controllers/admin.auth.controller.js
+++ controllers/admin.auth.controller.js
@@ -8,26 +8,26 @@ exports.register = (req, res) => {
         fullname: req.body.fullname,
         email: req.body.email,
         role: req.body.role,
-        password:  bcrypt.hashSync(req.body.password, 8)
+        password: bcrypt.hashSync(req.body.password, 8)
     };
 
     fs.readFile(`./admin.data/${user.email}.json`, (err, data) => {
         if (data != undefined) {
+            console.log("User already exists");
             res.status(409).send({ message: "User already exists." })
             return;
+        } else {
+            saveUserToFolder(user, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(404).send({ message: "User not saved." });
+                    return;
+                }
+                res.status(200).send({ message: "User registered successfully." });
+                console.log("New user registered");
+            });
         }
     })
-
-    saveUserToFolder(user, function(err) {
-        if (err) {
-            console.log(err);
-            res.status(404).send({ message: "User not saved." });
-            return;
-        }
-        res.status(200).send({ message: "User registered successfully." });
-        console.log("New user registered" );
-    });
-    
 };
 
 exports.login = (req, res) => {
@@ -55,12 +55,12 @@ exports.login = (req, res) => {
         if (!passwordValid) {
             console.log("Invalid password");
             return res.status(401)
-            .send({
-                accessToken: null,
-                message: "Invalid Password!"
-            });
+                .send({
+                    accessToken: null,
+                    message: "Invalid Password!"
+                });
         }
-    
+
         //signing token with user id
         var token = jwt.sign({
             email: user.email
@@ -70,15 +70,15 @@ exports.login = (req, res) => {
         });
 
         res.status(200)
-        .send({
-            user: {
-                email: user.email,
-                fullname: user.fullname,
-                role: user.role
-            },
-            message: "Login successful",
-            accessToken: token
-        });
+            .send({
+                user: {
+                    email: user.email,
+                    fullname: user.fullname,
+                    role: user.role
+                },
+                message: "Login successful",
+                accessToken: token
+            });
         console.log("User login successful");
 
     });
