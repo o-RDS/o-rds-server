33a7cace432c8788b4381c9462a2f46615ecf5aa
Morgan Jackson <mbj4school@gmail.com>
2023-02-23 13:32:21 -0600

    some validation + uses db for accounts

--- controllers/admin.auth.controller.js
+++ controllers/admin.auth.controller.js
@@ -1,89 +1,87 @@
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcrypt");
-var fs = require('fs');
+var fs = require("fs");
 const { getUser, postUser } = require("../database/firestoreFunctions.js");
 
 exports.register = (req, res) => {
+  if (
+    !req.body.fullname ||
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.role
+  ) {
+    console.log("Missing fields");
+    res.status(400).send({ message: "Missing fields" });
+    return;
+  }
 
-    var user = {
-        fullname: req.body.fullname,
-        email: req.body.email,
-        role: req.body.role,
-        password: bcrypt.hashSync(req.body.password, 8)
-    };
+  var user = {
+    fullname: req.body.fullname,
+    email: req.body.email,
+    role: req.body.role,
+    password: bcrypt.hashSync(req.body.password, 8),
+    surveys: [],
+  };
 
-    fs.readFile(`./admin.data/${user.email}.json`, (err, data) => {
-        if (data != undefined) {
-            console.log("User already exists");
-            res.status(409).send({ message: "User already exists." })
-            return;
-        } else {
-            saveUserToFolder(user, function (err) {
-                if (err) {
-                    console.log(err);
-                    res.status(404).send({ message: "User not saved." });
-                    return;
-                }
-                res.status(201).send({ message: "User registered successfully." });
-                console.log("New user registered");
-            });
-        }
-    })
+  let status = postUser(user);
+  if (status === 201) {
+    console.log("User created");
+    res.status(201).send({ message: "User created" });
+    return;
+  } else if (status === 409) {
+    console.log("User already exists");
+    res.status(409).send({ message: "User already exists" });
+    return;
+  } else {
+    console.log("Internal server error");
+    res.status(500).send({ message: "Internal server error" });
+    return;
+  }
 };
 
 exports.login = (req, res) => {
-    fs.readFile(`./admin.data/${req.body.email}.json`, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send(err);
-            return;
-        }
+  user = getUser(req.body.email);
+  if (user === undefined) {
+    console.log("Internal server error");
+    res.status(500).send({ message: "Internal server error" });
+    return;
+  }
+  else if (user === 404) {
+    console.log("User not found");
+    res.status(404).send({ message: "Email and password combination invalid." });
+    return;
+  }
 
-        var user = JSON.parse(data);
-        if (!user) {
-            console.log("User not found");
-            return res.status(404).send({
-                message: "Email and password combination invalid."
-            });
-        }
-
-        // compare passwords
-        var passwordValid = bcrypt.compareSync(
-            req.body.password,
-            user.password
-        );
-        // checking if password was valid and send response accordingly
-        if (!passwordValid) {
-            console.log("Invalid password");
-            return res.status(404).send({
-                message: "Email and password combination invalid."
-            });
-        }
-
-        //signing token with user id
-        var token = jwt.sign({
-            email: user.email
-        }, process.env.JWT_API_SECRET, {
-            // in seconds (24 hours)
-            expiresIn: 86400
-        });
-
-        res.status(200)
-            .send({
-                user: {
-                    email: user.email,
-                    fullname: user.fullname,
-                    role: user.role
-                },
-                message: "Login successful",
-                accessToken: token
-            });
-        console.log("User login successful");
-
+  // compare passwords
+  var passwordValid = bcrypt.compareSync(req.body.password, user.password);
+  // checking if password was valid and send response accordingly
+  if (!passwordValid) {
+    console.log("Invalid password");
+    return res.status(404).send({
+      message: "Email and password combination invalid.",
     });
+  }
 
+  //signing token with user id
+  var token = jwt.sign(
+    {
+      email: user.email,
+    },
+    process.env.JWT_API_SECRET,
+    {
+      // in seconds (24 hours)
+      expiresIn: 86400,
+    }
+  );
+
+  res.status(200).send({
+    user: {
+      email: user.email,
+      fullname: user.fullname,
+      role: user.role,
+    },
+    message: "Login successful",
+    accessToken: token,
+  });
+  console.log("User login successful");
 };
-
-function saveUserToFolder(user, callback) {
-    fs.writeFile(`./admin.data/${user.email}.json`, JSON.stringify(user), callback);
-}
